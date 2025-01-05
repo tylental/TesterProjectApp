bf4080bbde7fbf4358b5a03981f0ca11583ce9b5
tylental <101352437+tylental@users.noreply.github.com>
2025-01-05 14:01:53 -0500

    Initial Commit

---
+++ TesterProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TesterProject
+//
+//  Created by Tyler Diep on 2025-01-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TesterProject/TesterProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesterProjectApp.swift
+//  TesterProject
+//
+//  Created by Tyler Diep on 2025-01-05.
+//
+
+import SwiftUI
+
+@main
+struct TesterProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
